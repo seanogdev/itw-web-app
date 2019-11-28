8499315299d3e22a73badba27619c282a6695172
Sean O'Grady <sean@miralize.com>
2019-11-28 21:58:34 +0000

    pagintion

---
+++ src/views/CategorySingle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <!-- Featured -->
+
+    <!-- Dept Specific -->
+
+    <!-- Paginated -->
+    <PostList
+      :query="query"
+      :query-variables="{ slug: $route.params.slug}"
+    />
+  </div>
+</template>
+
+<script>
+
+// @ is an alias to /src
+import PostList from '@/components/PostList.vue';
+import { GET_POSTS_QUERY_CATEGORIES } from '../queries/posts';
+
+export default {
+  components: {
+    PostList,
+  },
+  data() {
+    return {
+      query: GET_POSTS_QUERY_CATEGORIES,
+    };
+  },
+};
+</script>
