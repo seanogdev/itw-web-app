217e1e96b7e2587dfe92f118323c681c7c4a8ae7
Sean O'Grady <sean@miralize.com>
2019-11-29 20:00:11 +0000

    realtime search

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
-console.log(process.env.VUE_APP_GRAPHQL_ENDPOINT);
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
   uri: process.env.VUE_APP_GRAPHQL_ENDPOINT,
